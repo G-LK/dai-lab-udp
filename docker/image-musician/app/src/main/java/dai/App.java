9e96e478f97e6a8b3ad16cd03bb33e37a4d05f1e
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2024-01-25 11:47:46 +0100

    First version of image-musician

--- docker/image-musician/app/src/main/java/dai/App.java
+++ docker/image-musician/app/src/main/java/dai/App.java
@@ -3,12 +3,28 @@
  */
 package dai;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import java.util.UUID;
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+public class App {
+    public static void main(String[] args) throws InterruptedException {
+        String uuid = UUID.randomUUID().toString();
+
+        if (args.length == 0) {
+            System.err.println("Error: you need to give the instrument name as the first argument");
+            System.exit(1);
+        }
+        if (!Musician.soundsByInstrument.containsKey(args[0])) {
+            System.err.println("Error: on the following instruments are valid: " + Musician.soundsByInstrument);
+            System.exit(1);
+        }
+
+        System.out.println("Musician started " + uuid + "!");
+
+        Musician musician = new Musician(uuid, args[0]);
+
+        while (true) {
+            Thread.sleep(1000);
+            musician.play();
+        }
     }
 }
---
+++ docker/image-musician/app/src/main/java/dai/Musician.java
@@ -0,0 +1,57 @@
+package dai;
+
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+import static java.nio.charset.StandardCharsets.*;
+
+public class Musician {
+    final static String IPADDR = "239.255.22.5";
+    final static int UDP_PORT = 9904;
+    final String uuid;
+    final String sound;
+
+    static Map<String, String> soundsByInstrument = new HashMap<>();
+    static {
+        soundsByInstrument.put("piano", "ti-ta-ti");
+        soundsByInstrument.put("trumpet", "pouet");
+        soundsByInstrument.put("flute", "trulu");
+        soundsByInstrument.put("violin", "gzi-gzi");
+        soundsByInstrument.put("drum", "boum-boum");
+    }
+
+    Musician(String uuid, String instrument) {
+        this.uuid = uuid;
+        this.sound = soundsByInstrument.get(instrument);
+    }
+
+    record Sound(String uuid, String sound) {
+    }
+
+    void play() {
+        try (DatagramSocket socket = new DatagramSocket()) {
+            socket.setBroadcast(true);
+
+            Sound soundEvent = new Sound(uuid, sound);
+
+            ObjectWriter ow = new ObjectMapper().writer();
+            String json = ow.writeValueAsString(soundEvent);
+
+            byte[] payload = json.getBytes(UTF_8);
+            var dest_address = new InetSocketAddress(IPADDR, UDP_PORT);
+            var packet = new DatagramPacket(payload, payload.length, dest_address);
+            socket.send(packet);
+            System.out.println("Played " + sound + " by " + uuid);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
